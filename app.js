dd9d0a0eff26fed0426ae31abccc6c8294a09bfa
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-05-22 20:25:54 +0100

    app.js modified

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
-
 const port = process.env.PORT || 3000;
+
+//Parsing middleware
+//Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//Parse application/json
+app.use(bodyParser.json());
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
