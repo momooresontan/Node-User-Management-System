1b0e80784147e55b54fea53a1754eb26118e1191
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-05-23 02:19:37 +0100

    app.js modified, connection to db

--- app.js
+++ app.js
@@ -29,6 +29,13 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+});
+
+//Connect to DB
+pool.getConnection((err, connection) => {
+  if (err) throw err; // not connected
+  console.log(`Connected as ID ${connection.threadId}`);
 });
 
 //Router
