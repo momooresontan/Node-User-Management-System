d8dda83a9379623430267888b06aa1d711b1b35e
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-05-25 08:16:40 +0100

    viewuser hbs modified

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -6,7 +6,6 @@ const router = express.Router();
 //Create, find, update, delete
 router.get("/", userController.overview);
 router.post("/", userController.find);
-router.get("/:id", userController.delete);
 
 router.get("/adduser", userController.newUser);
 router.post("/adduser", userController.create);
@@ -15,5 +14,7 @@ router.get("/edituser/:id", userController.edit);
 router.post("/edituser/:id", userController.update);
 
 router.get("/viewuser/:id", userController.view);
+
+router.get("/:id", userController.delete);
 
 module.exports = router;
